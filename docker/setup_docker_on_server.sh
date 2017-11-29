d98edcae622e6655e1e7d9d7b4d232658929f9d3
Smite Chow <xiaopengyou2no1@gmail.com>
2017-11-29 10:23:09 +0800

    fix ubuntu 16.04 not set docker registry mirror

--- docker/setup_docker_on_server.sh
+++ docker/setup_docker_on_server.sh
@@ -8,5 +8,18 @@ sudo apt-get update
 sudo apt-get -y install linux-image-extra-$(uname -r)
 sudo apt-get -y install linux-image-extra-virtual
 sudo apt-get -y install docker-engine
-sudo sed -in "s/^.*DOCKER_OPTS.*$/DOCKER_OPTS=\"--registry-mirror=https:\/\/8i15ehg9.mirror.aliyuncs.com\"/g" /etc/default/docker
-sudo restart docker
+
+if [ ! -f /bin/systemctl ]; then
+    sudo sed -in "s/^.*DOCKER_OPTS.*$/DOCKER_OPTS=\"--registry-mirror=https:\/\/registry.docker-cn.com\"/g" /etc/default/docker
+    sudo restart docker
+else
+    sudo cat>/etc/docker/daemon.json<<EOF
+{
+  "registry-mirrors": [
+    "https://registry.docker-cn.com"
+  ]
+}
+EOF
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+fi
